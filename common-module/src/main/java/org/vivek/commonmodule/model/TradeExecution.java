09fee3bb7ca6501abe6ecf05776498e03b1ea202
Lenovo <vivekmane9860@gmail.com>
2026-04-04 11:22:55 +0530

    feat(common): add core domain models (Order, TaskResult, DAGResult) and update trade entities

--- common-module/src/main/java/org/vivek/commonmodule/model/TradeExecution.java
+++ common-module/src/main/java/org/vivek/commonmodule/model/TradeExecution.java
@@ -15,6 +15,8 @@ public class TradeExecution {
     private String tradeId;
     private String buyOrderId;
     private String sellOrderId;
+    private String buyerId;
+    private String sellerId;
     private String symbol;
     private double quantity;
     private double executedPrice;
