b9e11eda1a09f0a51e9e3fc26907b55175aca6f0
Lenovo <vivekmane9860@gmail.com>
2026-04-07 15:23:13 +0530

    price tick and price simulation

---
+++ market-data-service/src/main/java/org/vivek/marketdataservice/model/PriceTick.java
@@ -0,0 +1,21 @@
+package org.vivek.marketdataservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PriceTick {
+    private String symbol;
+    private double price;
+    private double change;
+    private double changePercent;
+    private Instant timestamp;
+    private int volume;
+}
