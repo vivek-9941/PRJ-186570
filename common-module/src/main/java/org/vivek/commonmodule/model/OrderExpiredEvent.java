893d2f78b7dd6c4deb159fb3fe8e6bb3e456660d
Lenovo <vivekmane9860@gmail.com>
2026-04-07 15:25:27 +0530

    order expiry scheduler and event producing

---
+++ common-module/src/main/java/org/vivek/commonmodule/model/OrderExpiredEvent.java
@@ -0,0 +1,19 @@
+package org.vivek.commonmodule.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderExpiredEvent {
+    private String orderId;
+    private String userId;
+    private String symbol;
+    private Instant expiredAt;
+}
